5e27abbe6d7ec3bb6f93a5e8ced934f9f3311592
Molly Fuhrman <mollyfuhrman@gmail.com>
2022-10-12 13:33:24 -0700

    Added social media icons w/ transitions. Added Draggable. Added icons for social media.

--- src/Tedious.jsx
+++ src/Tedious.jsx
@@ -2,18 +2,37 @@ import React from 'react';
 import { useState } from 'react'
 // import TediousBgImg from './TediousBgImg.jpg';
 import Tilt from 'react-tilt';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import Draggable from 'react-draggable';
 
 export default function Tedious() {
   return (
     <>
     <div className="divWrapper">
       <div className="tedious_bg" style={{display: 'flex', justifyContent: 'center'}}>
+            <span className="found_me">
+              you found me! :D
+            </span>
           {/* <Tilt className="background-circle-wrapper" style={{maxWidth: 'fit-content'}} options={{ max : 10, reset: true }}> */}
-         <div>
-            <span>facebook</span>
-            <span>instagram</span>
+         <div className="socialMediaIconsWrapper">
+          <a href="https://youtube.com" target="_blank" rel="noreferrer">
+            <YouTubeIcon className="socialMediaIcons"></YouTubeIcon>
+            </a>
+          <a href="fhttps://acebook.com" target="_blank" rel="noreferrer">
+            <FacebookIcon className="socialMediaIcons"></FacebookIcon>
+            </a>
+            <a href="https://instagram.com" target="_blank" rel="noreferrer">
+            <InstagramIcon className="socialMediaIcons"></InstagramIcon>
+            </a>
+            {/* <span className="social_media_icons instagram">{FacebookIcon}</span> */}
          </div>
+        <Draggable>
           <span className="background-circle-wrapper">
+            {/* <span className="loading">
+              loading: good vibes....
+            </span> */}
             <span className="background-circle">
               <span className="T-Top" />
               <span className="T-Side" />
@@ -38,6 +57,7 @@ export default function Tedious() {
             </span>
           </span>
           {/* </Tilt> */}
+        </Draggable>
           <iframe className="live_music_soundcloud" title="tedious_song" width="100%" height="166" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/950548042&color=%23c8afb2&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
           <div Style={{fontSize: '10px', color: '#c8afb2',lineBreak: 'anywhere',wordBreak: 'normal',overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis', fontFamily: 'Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif',fontWeight: '100'}}>
             {/* <a href="https://soundcloud.com/user-374679796" title="Erik Genaff" target="_blank" rel="noreferrer" Style={{color: '#c8afb2', textDecoration: 'none'}}>Erik Genaff</a> ·
