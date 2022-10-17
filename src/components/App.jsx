0d114d0b5f5bac1f4fdd072d17a5224b96b37eda
molfuh <mollyfuhrman@gmail.com>
2022-10-16 18:46:41 -0700

    Removed commented-out code.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,4 @@
-import React from 'react';
-// import logo from './logo.svg';
 import '../styles/App.scss';
-// import './AppMediaQuery.scss';
-// import './styleVariables.scss';
 import Tedious from './Tedious';
 
 
@@ -12,24 +8,6 @@ function App() {
         <Tedious />
       </>
   );
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.tsx</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
 }
 
 export default App;
