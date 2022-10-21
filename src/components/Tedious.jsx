487ac514fdaae7d134d15a4f308cd888989738ab
molfuh <mollyfuhrman@gmail.com>
2022-10-21 09:28:31 -0700

    Added buttons

--- src/components/Tedious.jsx
+++ src/components/Tedious.jsx
@@ -4,27 +4,27 @@ import Draggable from 'react-draggable';
 import { ReactComponent as TikTokIcon } from '../images/tiktok.svg';
 import React from 'react';
 
-// export default function Tedious() {
-class Tedious extends React.Component {
-  state = {
-    left: 0,
-    top: 0
-}
-componentDidMount() {
-    document.addEventListener("mousemove", this.handleCursor);
-}
-componentWillUnmount() {
-  // Make sure to remove the DOM listener when the component is unmounted.
-  document.removeEventListener("mousemove", this.handleCursor);
-}
-handleCursor = (e) => {
-    this.setState({left: e.pageX, top: e.pageY});
-    console.log(this.state.left, this.state.right);
-}
+export default function Tedious() {
+// class Tedious extends React.Component {
+//   state = {
+//     left: 0,
+//     top: 0
+// }
+// componentDidMount() {
+//     document.addEventListener("mousemove", this.handleCursor);
+// }
+// componentWillUnmount() {
+//   // Make sure to remove the DOM listener when the component is unmounted.
+//   document.removeEventListener("mousemove", this.handleCursor);
+// }
+// handleCursor = (e) => {
+//     this.setState({left: e.pageX, top: e.pageY});
+//     console.log(this.state.left, this.state.right);
+// }
   // const cursor = document.getElementsByClassName('cursor')[0];     
   // cursor.setAttribute('style','top:'+e.pageY+'px; left:'+ e.pageX+'px;');
  
- render() {
+//  render() {
   return (
     <>
       <div className="appWrapper">
@@ -32,6 +32,20 @@ handleCursor = (e) => {
           <span className="found_me">
               you found me! :D
           </span>
+         <div className="buttonsIconsWrapper">
+            <button className="bio buttons">About</button>
+            {/* <button href="#" clbuttonss="buttons">
+              <span>About</span>
+              <svg width="13px" height="10px" viewBox="0 0 13 10">
+                <path d="M1,5 L11,5"></path>
+                <polyline points="8 1 12 5 8 9"></polyline>
+              </svg>
+            </button> */}
+            {/* </button> */}
+            <button className="listen buttons">Listen</button>
+            <button className="contact buttons">Contact</button>
+            
+          </div>
          <div className="socialMediaIconsWrapper">
             <a href="https://www.youtube.com/channel/UC7SRMEAzVHVG5gHimUlEHDg" target="_blank" rel="noreferrer">
               <YouTubeIcon className="socialMediaIcons" />
@@ -45,7 +59,8 @@ handleCursor = (e) => {
           </div>
           <Draggable>
             <span className="background-circle-wrapper">
-              <span className="background-circle" style={{left: this.state.left, top: this.state.top}} >
+              {/* <span className="background-circle" style={{left: this.state.left, top: this.state.top}} > */}
+              <span className="background-circle" >
                 <span className="T-Side" />
                 <span className="T-Top" />
                 <div className="HorizontalLinesWrapper">
@@ -72,7 +87,7 @@ handleCursor = (e) => {
       </div>
     </>
   )
-}
+// }
 }
 
-export default Tedious;
+// export default Tedious;
